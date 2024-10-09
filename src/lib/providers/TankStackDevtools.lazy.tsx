4a15eb4878a5bb4b7f0971f0f61b7074456c2c34
Roderick <rromero@ata-soft.com>
2024-10-09 16:57:21 -0500

    base confiuuguration shadcn tanstack router and query

---
+++ src/lib/providers/TankStackDevtools.lazy.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const TanStackRouterDevtoolsLazy =
+    process.env.NODE_ENV === 'production'
+        ? () => null // Render nothing in production
+        : React.lazy(() =>
+            // Lazy load in development
+            import('@tanstack/router-devtools').then((res) => ({
+                default: res.TanStackRouterDevtools,
+                // For Embedded Mode
+                // default: res.TanStackRouterDevtoolsPanel
+            })),
+        )
+
+export const ReactQueryDevtoolsLazy = process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/react-query-devtools').then((res) => ({
+            default: res.ReactQueryDevtools,
+            // For Embedded Mode
+            // default: res.TanStackRouterDevtoolsPanel
+        })),
+    )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,57 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+import { ReactQueryDevtoolsLazy, TanStackRouterDevtoolsLazy } from './lib/providers/TankStackDevtools.lazy'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+
+const queryClient = new QueryClient();
+
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+    //you can add more properties here in order to have types globaly and acces to conext example : auth: {} as AuthContext,
+  },
+  defaultPreload: "intent",
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+});
+
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+
+// function InnerApp() {
+// 	const auth = useAuth();
+// 	return <RouterProvider router={router} context={{ auth }} />;
+// }
+
+
+// Render the app
+const rootElement = document.getElementById('root') as HTMLDivElement
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtoolsLazy initialIsOpen={false} />
+        <TanStackRouterDevtoolsLazy router={router} initialIsOpen={false} />
+      </QueryClientProvider>
+    </StrictMode>,
+  )
+}
